6ef20a0ce1859eb16bff30f5b5aa8be9771490b8
pvsteja <tejapathri6@msitprogram.net>
2018-09-27 00:11:42 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- ADS1 Assignments/M3/Assignment1/3-sum/Solution.java
+++ ADS1 Assignments/M3/Assignment1/3-sum/Solution.java
@@ -1,35 +1,49 @@
 import java.util.Scanner;
 import java.util.Arrays;
-/**
+/**.
  * Class for solution.
  */
 public final class Solution {
-    /**
-     * Constructs the object.
-     */
-    private Solution() {
-    }
-    /**
-     * main.
+    /**.
+     * { function_description }
      *
      * @param      args  The arguments
      */
-    public static void main(final String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] a = new int[n];
-        for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
+    /**
+         * Constructs the object.
+         */
+        private Solution() {
+
         }
+        /**.
+         * { function_description }
+         *
+         * @param      args  The arguments
+         */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        int k, n;
         int count = 0;
-        int j, k;
+        n = s.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = s.nextInt();
+        }
         Arrays.sort(a);
-        for (int i = 0; i < n; i++) {
-            for (j = i + 1; j < n; j++) {
-                for (k = j + 1; k < n; k++) {
-                    if (a[i] + a[j] + a[k] == 0) {
-                        count++;
-                    }
+        for (int i = 0; i < n - 2; i++) {
+            int j = i + 1;
+            int x = n - 1;
+            while (j < x) {
+                if (a[i] + a[j] + a[x] == 0) {
+                    count++;
+                    j++;
+                    x--;
+
+                } else if (a[i] + a[j] + a[x] < 0) {
+                    j++;
+
+                } else {
+                    x--;
                 }
             }
         }
