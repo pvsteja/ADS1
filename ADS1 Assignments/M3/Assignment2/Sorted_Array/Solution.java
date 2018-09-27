20d0f7e422c272e73b13f8944bbc02ab0f05d1e9
pvsteja <tejapathri6@msitprogram.net>
2018-09-27 15:57:19 +0530

    "check_style_score -> 3 of 3 passed. style score: 1/1 "

--- ADS1 Assignments/M3/Assignment2/Sorted_Array/Solution.java
+++ ADS1 Assignments/M3/Assignment2/Sorted_Array/Solution.java
@@ -1,68 +1,88 @@
 import java.util.Scanner;
 /**
- * Class for solution.
+ *Solution class.
+ **/
+
+public final class  Solution {
+
+/**
+ *Solution class.
  */
-public final class Solution {
-    /**
-     * Constructs the object.
+private Solution() {
+
+}
+
+/**.
+     * { Merge Arrays }
+     * @param      arr1     { parameter_description }
+     * @param      arr2     { parameter_description }
      */
-    private Solution() {
-        //constructor.
-    }
-    /**
-     * main function.
-     *
-     * @param      args  The arguments
-     */
-    public static void main(final String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt();
-        int n = sc.nextInt();
-        sc.nextLine();
-        int[] a = new int[m];
-        int[] b = new int[n];
-        int k = 0, i = 0, j = 0;
-        String temp = "";
-        String[] s;
-        if (m != 0) {
-            s = sc.nextLine().split(",");
-            for (String each : s) {
-                a[k++] = Integer.parseInt(each);
-            }
-        } else {
-            sc.nextLine();
-        }
-        k = 0;
-        if (n != 0) {
-            s = sc.nextLine().split(",");
-            // System.out.println("String .." + s);
-            for (String each : s) {
-                b[k++] = Integer.parseInt(each);
-            }
-        } else {
-            sc.nextLine();
-        }
-        while (i < m && j < n) {
-            if (a[i] < b[j]) {
-                temp += Integer.toString(a[i]) + ",";
+    public void mergeArrays(final int[] arr1, final int[] arr2) {
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int[] arr3 = new int[arr1.length + arr2.length];
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                arr3[k++] = arr1[i];
                 i++;
-            } else if (a[i] > b[j]) {
-                temp += Integer.toString(b[j]) + ",";
-                j++;
             } else {
-                temp += Integer.toString(b[j]) + ",";
+                arr3[k++] = arr2[j];
                 j++;
-                i++;
             }
         }
-        while (i < m) {
-            temp += Integer.toString(a[i]) + ",";
-            i++;
+        if (i == arr1.length) {
+            while (j < arr2.length) {
+                arr3[k++] = arr2[j];
+                j++;
+            }
+        } else {
+            while (i < arr1.length) {
+                arr3[k++] = arr1[i];
+                i++;
+
+            }
         }
-        while (j < n) {
-            temp += Integer.toString(b[j]) + ",";
-            j++;
+        int x;
+        for (x = 0; x < arr3.length - 1; x++) {
+            System.out.print(arr3[x] + ",");
         }
-        System.out.println(temp.substring(0, temp.length() - 1));
+        System.out.println(arr3[x]);
+    }
+
+/**
+ * @brief [brief description]
+ * @details [long description]
+ *
+ * @param args arguments
+ */
+    public static void main(final String[] args) {
+
+/**
+ * @brief [brief description]
+ * @details [long description]
+ */
+        Scanner sc = new Scanner(System.in);
+        int m = Integer.parseInt(sc.nextLine());
+        int n = Integer.parseInt(sc.nextLine());
+        int[] arr1 = new int[m];
+        int[] arr2 = new int[n];
+        String[] elements;
+        if (m != 0) {
+            elements = sc.nextLine().split(",");
+            for (int i = 0; i < elements.length; i++) {
+                arr1[i] = Integer.parseInt(elements[i]);
+            }
+        } else {
+            sc.nextLine();
+        }
+        if (n != 0) {
+            elements = sc.nextLine().split(",");
+            for (int i = 0; i < elements.length; i++) {
+                arr2[i] = Integer.parseInt(elements[i]);
+            }
+        }
+        new Solution().mergeArrays(arr1, arr2);
     }
 }
