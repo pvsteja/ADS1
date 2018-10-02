d0cfae7095462d64156edaae366fa937d4f63253
pvsteja <tejapathri6@msitprogram.net>
2018-10-03 00:00:49 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- ADS1 Assignments/M8/8.2 Team Ranking With Selection/8.2 Team Ranking With Selection/InsertionSort.java
+++ ADS1 Assignments/M8/8.2 Team Ranking With Selection/8.2 Team Ranking With Selection/InsertionSort.java
@@ -10,10 +10,10 @@ class InsertionSort {
 	public static void sort(final Comparable[] a) {
 		int N = a.length;
 		for (int i = 0; i < N; i++) {
-			for (int j = i + 1; j < N; j++) {
-				if (less(a[j], a[j-1])) {
-					exch(a,j,j-1);
-				}else break;
+			for (int j = i; j > 0; j--) {
+				if (less(a[j], a[j - 1])) {
+					exch(a, j, j - 1);
+				} else break;
 			}
 		}
 	}
@@ -26,19 +26,19 @@ class InsertionSort {
 	 * @return     { description_of_the_return_value }
 	 */
 	private static boolean less(final Comparable i, final Comparable j) {
-        return i.compareTo(j) < 0;
-    }
-    /**
-     * { function_description }
-     *
-     * @param      a     { parameter_description }
-     * @param      i     { parameter_description }
-     * @param      j     { parameter_description }
-     */
-    private static void exch(final Comparable[] a,
-        final int i, final int j) {
-        Comparable swap = a[i];
-        a[i] = a[j];
-        a[j] = swap;
-    }
+		return i.compareTo(j) < 0;
+	}
+	/**
+	 * { function_description }
+	 *
+	 * @param      a     { parameter_description }
+	 * @param      i     { parameter_description }
+	 * @param      j     { parameter_description }
+	 */
+	private static void exch(final Comparable[] a,
+	                         final int i, final int j) {
+		Comparable swap = a[i];
+		a[i] = a[j];
+		a[j] = swap;
+	}
 }
