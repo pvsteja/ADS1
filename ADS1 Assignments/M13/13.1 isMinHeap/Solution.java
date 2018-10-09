5287442e3fec858f602e7d6dad654ab94db84012
pvsteja <tejapathri6@msitprogram.net>
2018-10-10 00:07:20 +0530

    "ads1_m13_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS1 Assignments/M13/13.1 isMinHeap/Solution.java
+++ ADS1 Assignments/M13/13.1 isMinHeap/Solution.java
@@ -1,4 +1,4 @@
-/**
+/**.
  * import Scanner package
  */
 import java.util.Scanner;
@@ -6,58 +6,60 @@ import java.util.Scanner;
  * final class for Solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object for Solution.
-	 *
-	 */
-	private Solution() {
-		//empty constructor.
-	}
-	/**
-	 * { main function_description }
-	 * consists of Scanner package
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String s = scan.nextLine();
-		int n = Integer.parseInt(scan.nextLine());
-		switch (s) {
-			case "String" :
-			while (n > 0) {
-				String[] tokens = scan.nextLine().split(",");
-				MinPQ<String> minimum = new
-				MinPQ<String>(tokens);
-				System.out.println(minimum.isMinPQ());
-				n--;
-			}
-			break;
-			case "Integer":
-			while (n > 0) {
-				String[] tokens = scan.nextLine().split(",");
-				Integer[] integer = new Integer[tokens.length];
-				for (int i = 0; i < tokens.length; i++) {
-					integer[i] = Integer.parseInt(tokens[i]);
-				}
-				MinPQ<Integer> minimum = new
-				MinPQ<Integer>(integer);
-				System.out.println(minimum.isMinPQ());
-				n--;
-			}
-			break;
-			case "Double":
-			while (n > 0) {
-				String[] tokens = scan.nextLine().split(",");
-				Double[] doubtok = new Double[tokens.length];
-				for (int i = 0; i < tokens.length; i++) {
-					doubtok[i] = Double.parseDouble(tokens[i]);
-				}
-				MinPQ<Double> minimum = new
-				MinPQ<Double>(doubtok);
-				System.out.println(minimum.isMinPQ());
-				n--;
-			}
-			break;
+  /**.
+   * Constructs the object for Solution.
+   *
+   */
+  private Solution() {
+    //empty constructor.
+  }
+  /**.
+   * { main function_description }
+   * consists of Scanner package
+   * @param      args  The arguments
+   */
+  public static void main(final String[] args) {
+    Scanner scan = new Scanner(System.in);
+    String s = scan.nextLine();
+    int n = Integer.parseInt(scan.nextLine());
+    switch (s) {
+      case "String" :
+      while (n > 0) {
+        String[] tokens = scan.nextLine().split(",");
+        MinPQ<String> minimum = new
+        MinPQ<String>(tokens);
+        System.out.println(minimum.isMinPQ());
+        n--;
+      }
+      break;
+      case "Integer":
+      while (n > 0) {
+        String[] tokens = scan.nextLine().split(",");
+        Integer[] integer = new Integer[tokens.length];
+        for (int i = 0; i < tokens.length; i++) {
+          integer[i] =
+          Integer.parseInt(tokens[i]);
+        }
+        MinPQ<Integer> minimum = new
+        MinPQ<Integer>(integer);
+        System.out.println(minimum.isMinPQ());
+        n--;
+      }
+      break;
+      case "Double":
+      while (n > 0) {
+        String[] tokens = scan.nextLine().split(",");
+        Double[] doubtok = new Double[tokens.length];
+        for (int i = 0; i < tokens.length; i++) {
+          doubtok[i] =
+          Double.parseDouble(tokens[i]);
+        }
+        MinPQ<Double> minimum = new
+        MinPQ<Double>(doubtok);
+        System.out.println(minimum.isMinPQ());
+        n--;
+      }
+      break;
             case "Float":
             while (n > 0) {
                 String str = scan.nextLine();
@@ -83,6 +85,6 @@ public final class Solution {
             break;
             default:
             break;
-		}
-	}
+    }
+  }
 }
