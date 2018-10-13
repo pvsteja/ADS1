bb7a0a68bddb4ad7b98b287147e0938b87e83004
pvsteja <tejapathri6@msitprogram.net>
2018-10-13 12:33:13 +0530

    "ads1_week3_exam: 2/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS1 Assignments/M18/Week-3_Assignment-1/MaxPQ.java
@@ -0,0 +1,188 @@
+/**.
+ * importing comparator package.
+ */
+import java.util.Comparator;
+/**.
+ * importing ArrayList package.
+ */
+import java.util.ArrayList;
+/**
+ * Class for maximum pq.
+ *
+ * @param      <Key>  The key
+ */
+public class MaxPQ<Key> {
+    /**
+     * stores items from 1 to n.
+     */
+    private Key[] pq;
+    /**
+     * Number of items on priority queue.
+     */
+    private int n;
+    /**
+     * Comparator variable.
+     */
+    private Comparator<Key> comparator;
+
+    /**
+     * Initializes an empty priority queue with the given initial capacity.
+     *
+     * @param  initCapacity the initial capacity of this priority queue
+     */
+    public MaxPQ(final int initCapacity) {
+        pq = (Key[]) new Object[initCapacity + 1];
+        n = 0;
+    }
+
+    /**
+     * Initializes an empty priority queue.
+     */
+    public MaxPQ() {
+        this(1);
+    }
+
+    /**
+     * Returns true if this priority queue is empty.
+     *
+     * @return {@code true} if this priority queue is empty;
+     *         {@code false} otherwise
+     */
+    public boolean isEmpty() {
+        return n == 0;
+    }
+
+    /**
+     * Returns the number of keys on this priority queue.
+     *
+     * @return the number of keys on this priority queue
+     */
+    public int size() {
+        return n;
+    }
+
+    /**
+     * Returns a largest key on this priority queue.
+     *
+     * @return a largest key on this priority queue
+     */
+    public Key max() {
+        return pq[1];
+    }
+
+    /**
+     * Helper function to double the size of the heap array.
+     *
+     * @param      capacity  The capacity
+     */
+    private void resize(final int capacity) {
+        Key[] temp = (Key[]) new Object[capacity];
+        for (int i = 1; i <= n; i++) {
+            temp[i] = pq[i];
+        }
+        pq = temp;
+    }
+
+
+    /**
+     * Adds a new key to this priority queue.
+     *
+     * @param  x the new key to add to this priority queue
+     */
+    public void insert(final Key x) {
+        if (n == pq.length - 1) {
+            resize(2 * pq.length);
+        }
+        pq[++n] = x;
+        swim(n);
+    }
+
+    /**
+     * Removes and returns a largest key
+     * on this priority queue.
+     *
+     * @return a largest key on this priority queue
+     */
+    public Key delMax() {
+        final int four = 4;
+        Key max = pq[1];
+        exch(1, n--);
+        sink(1);
+        pq[n + 1] = null;
+        // to avoid loiterig and help with garbage collection
+        if ((n > 0) && (n == (pq.length - 1) / four)) {
+            resize(pq.length / 2);
+        }
+        return max;
+    }
+
+    /**
+    * Swim function.
+    *
+    * @param      k1     { parameter_description }
+    */
+    private void swim(final int k1) {
+        int k = k1;
+        while (k > 1 && less(k / 2, k)) {
+            exch(k, k / 2);
+            k = k / 2;
+        }
+    }
+    /**
+     * Sink function.
+     *
+     * @param      k1     { parameter_description }
+     */
+    private void sink(final int k1) {
+        int k = k1;
+        while (2 * k <= n) {
+            int j = 2 * k;
+            if (j < n && less(j, j + 1)) {
+                j++;
+            }
+            if (!less(k, j)) {
+                break;
+            }
+            exch(k, j);
+            k = j;
+        }
+    }
+
+    /**
+     * Less than function.
+     *
+     * @param      i     { parameter_description }
+     * @param      j     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    private boolean less(final int i, final int j) {
+        if (comparator == null) {
+            return ((Comparable<Key>) pq[i]).compareTo(pq[j]) < 0;
+        } else {
+            return comparator.compare(pq[i], pq[j]) < 0;
+        }
+    }
+    /**
+     * Swaps the values in the array.
+     *
+     * @param      i     { parameter_description }
+     * @param      j     { parameter_description }
+     */
+    private void exch(final int i, final int j) {
+        Key swap = pq[i];
+        pq[i] = pq[j];
+        pq[j] = swap;
+    }
+    /**
+    * Prints the maximum 5 of the PQ.
+    */
+    public void print5(final ArrayList<Key> best) {
+        for (int i = 1; i <= 5; i++) {
+            Key temp = delMax();
+            System.out.println(temp);
+            best.add(temp);
+        }
+        System.out.print("\n");
+    }
+}
---
+++ ADS1 Assignments/M18/Week-3_Assignment-1/Solution.java
@@ -0,0 +1,199 @@
+/**.
+ * Importing the Scanner package.
+ */
+import java.util.Scanner;
+/**
+ * Importing ArrayList package.
+ */
+import java.util.ArrayList;
+
+/**
+ * Class for stock which implements comparable stock.
+ */
+class Stock implements Comparable<Stock> {
+	/**
+	 * Class variable for company name.
+	 */
+	private String company;
+	/**
+	 * Class variable for change in stock by hour.
+	 */
+	private double change;
+
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      cmp   The company
+	 * @param      chg   The change
+	 */
+	Stock(final String cmp, final double chg) {
+		this.company = cmp;
+		this.change = chg;
+	}
+
+	/**
+	 * Gets the company of type String.
+	 * returns company
+	 *
+	 * @return     The company.
+	 */
+	public String getComp() {
+		return company;
+	}
+
+	/**
+	 * Gets the change.
+	 *
+	 * @return     The change.
+	 */
+	public double getChange() {
+		return change;
+	}
+
+	/**
+	 * Returns a string representation of the object.
+	 *
+	 * @return     String representation of the object.
+	 */
+	public String toString() {
+		return company + " " + change;
+	}
+
+	/**
+	 * Compares two stocks.
+	 * Using compareTo method
+	 * it will compare to objects and returns the value.
+	 * @param      that  The that
+	 *
+	 * @return     Integer value to compare.
+	 */
+	public int compareTo(final Stock that) {
+		if (this.change > that.getChange()) {
+			return 1;
+		}
+		if (this.change < that.getChange()) {
+			return -1;
+		}
+		if (this.company.compareTo(that.getComp()) > 0) {
+			return 1;
+		}
+		if (this.company.compareTo(that.getComp()) < 0) {
+			return -1;
+		} else {
+			return 0;
+		}
+	}
+}
+
+/**
+ * Class for solution.
+ */
+public final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		// unused
+	}
+
+	/**
+	 * Main method - Takes Inputs, prints out Outputs.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int stockSize = Integer.parseInt(scan.nextLine());
+		ArrayList<Stock> bestPerformers = new ArrayList<Stock>();
+		ArrayList<Stock> worstPerformers = new ArrayList<Stock>();
+
+		// hour 1
+		MinPQ<Stock> hour1Min = new MinPQ<Stock>();
+		MaxPQ<Stock> hour1Max = new MaxPQ<Stock>();
+
+		for (int i = 0; i < stockSize; i++) {
+			String[] inputs = scan.nextLine().split(",");
+			Stock temp = new Stock(inputs[0],Double.parseDouble(inputs[1]));
+			hour1Max.insert(temp);
+			hour1Min.insert(temp);
+		}
+
+		hour1Max.print5(bestPerformers);
+		hour1Min.print5(worstPerformers);
+
+
+		// hour 2
+		MinPQ<Stock> hour2Min = new MinPQ<Stock>();
+		MaxPQ<Stock> hour2Max = new MaxPQ<Stock>();
+
+		for (int i = 0; i < stockSize; i++) {
+			String[] inputs = scan.nextLine().split(",");
+			Stock temp = new Stock(inputs[0],Double.parseDouble(inputs[1]));
+			hour2Max.insert(temp);
+			hour2Min.insert(temp);
+		}
+
+		hour2Max.print5(bestPerformers);
+		hour2Min.print5(worstPerformers);
+
+		// hour 3
+		MinPQ<Stock> hour3Min = new MinPQ<Stock>();
+		MaxPQ<Stock> hour3Max = new MaxPQ<Stock>();
+
+		for (int i = 0; i < stockSize; i++) {
+			String[] inputs = scan.nextLine().split(",");
+			Stock temp = new Stock(inputs[0],Double.parseDouble(inputs[1]));
+			hour3Max.insert(temp);
+			hour3Min.insert(temp);
+		}
+
+		hour3Max.print5(bestPerformers);
+		hour3Min.print5(worstPerformers);
+
+		// hour 4
+		MinPQ<Stock> hour4Min = new MinPQ<Stock>();
+		MaxPQ<Stock> hour4Max = new MaxPQ<Stock>();
+
+		for (int i = 0; i < stockSize; i++) {
+			String[] inputs = scan.nextLine().split(",");
+			Stock temp = new Stock(inputs[0],
+			                       Double.parseDouble(inputs[1]));
+			hour4Max.insert(temp);
+			hour4Min.insert(temp);
+		}
+
+		hour4Max.print5(bestPerformers);
+		hour4Min.print5(worstPerformers);
+
+		// hour 5
+		MinPQ<Stock> hour5Min = new MinPQ<Stock>();
+		MaxPQ<Stock> hour5Max = new MaxPQ<Stock>();
+
+		for (int i = 0; i < stockSize; i++) {
+			String[] inputs = scan.nextLine().split(",");
+			Stock temp = new Stock(inputs[0],
+			                       Double.parseDouble(inputs[1]));
+			hour5Max.insert(temp);
+			hour5Min.insert(temp);
+		}
+
+		hour5Max.print5(bestPerformers);
+		hour5Min.print5(worstPerformers);
+
+		// hour 6
+		MinPQ<Stock> hour6Min = new MinPQ<Stock>();
+		MaxPQ<Stock> hour6Max = new MaxPQ<Stock>();
+
+		for (int i = 0; i < stockSize; i++) {
+			String[] inputs = scan.nextLine().split(",");
+			Stock temp = new Stock(inputs[0],
+			                       Double.parseDouble(inputs[1]));
+			hour6Max.insert(temp);
+			hour6Min.insert(temp);
+		}
+
+		hour6Max.print5(bestPerformers);
+		hour6Min.print5(worstPerformers);
+	}
+}
