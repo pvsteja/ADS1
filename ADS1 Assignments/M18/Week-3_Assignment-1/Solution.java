f932cd334d165b0e3806ee08a8a911f3b057b216
pvsteja <tejapathri6@msitprogram.net>
2018-10-13 13:09:52 +0530

    "ads1_week3_exam: 2/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS1 Assignments/M18/Week-3_Assignment-1/Solution.java
+++ ADS1 Assignments/M18/Week-3_Assignment-1/Solution.java
@@ -111,88 +111,127 @@ public final class Solution {
     ArrayList<Stock> worstPerformers = new ArrayList<Stock>();
 
     // hour 1
+    //minPQ of companys stockdetails for 1st hour.
     MinPQ<Stock> hour1Min = new MinPQ<Stock>();
+    //maxPQ of companys stockdetails for 1st hour.
     MaxPQ<Stock> hour1Max = new MaxPQ<Stock>();
 
+
     for (int i = 0; i < stockSize; i++) {
+      //
       String[] inputs = scan.nextLine().split(",");
+      //Initialing temp and storing the company names and percentages.
       Stock temp = new Stock(inputs[0], Double.parseDouble(inputs[1]));
+      //inserting temp int max.
       hour1Max.insert(temp);
+      //inserting temp int min.
       hour1Min.insert(temp);
     }
 
+    //printing bestperformers for hour1.
     hour1Max.print(bestPerformers);
+    //printing worstperformers for hour1.
     hour1Min.print(worstPerformers);
 
 
     // hour 2
+    //minPQ of companys stockdetails for 2nd hour.
     MinPQ<Stock> hour2Min = new MinPQ<Stock>();
+    //maxPQ of companys stockdetails for 2nd hour.
     MaxPQ<Stock> hour2Max = new MaxPQ<Stock>();
 
     for (int i = 0; i < stockSize; i++) {
       String[] inputs = scan.nextLine().split(",");
+      //Initialing temp and storing the company names and percentages.
       Stock temp = new Stock(inputs[0], Double.parseDouble(inputs[1]));
+      //inserting temp int max.
       hour2Max.insert(temp);
+      //inserting temp int min.
       hour2Min.insert(temp);
     }
-
+    //printing bestperformers for hour2.
     hour2Max.print(bestPerformers);
+    //printing worstperformers for hour2.
     hour2Min.print(worstPerformers);
 
     // hour 3
+    //minPQ of companys stockdetails for 3rd hour.
     MinPQ<Stock> hour3Min = new MinPQ<Stock>();
+    //maxPQ of companys stockdetails for 3rd hour.
     MaxPQ<Stock> hour3Max = new MaxPQ<Stock>();
 
     for (int i = 0; i < stockSize; i++) {
       String[] inputs = scan.nextLine().split(",");
+      //Initialing temp and storing the company names and percentages.
       Stock temp = new Stock(inputs[0], Double.parseDouble(inputs[1]));
+      //inserting temp int max.
       hour3Max.insert(temp);
+      //inserting temp int min.
       hour3Min.insert(temp);
     }
-
+    //printing bestperformers for hour3.
     hour3Max.print(bestPerformers);
+    //printing worstperformers for hour3.
     hour3Min.print(worstPerformers);
 
     // hour 4
+    //minPQ of companys stockdetails for 4th hour.
     MinPQ<Stock> hour4Min = new MinPQ<Stock>();
+    //maxPQ of companys stockdetails for 4th hour.
     MaxPQ<Stock> hour4Max = new MaxPQ<Stock>();
 
     for (int i = 0; i < stockSize; i++) {
       String[] inputs = scan.nextLine().split(",");
+      //Initialing temp and storing the company names and percentages.
       Stock temp = new Stock(inputs[0], Double.parseDouble(inputs[1]));
+      //inserting temp int max.
       hour4Max.insert(temp);
+      //inserting temp int min.
       hour4Min.insert(temp);
     }
-
+    //printing bestperformers for hour4.
     hour4Max.print(bestPerformers);
+    //printing worstperformers for hour4.
     hour4Min.print(worstPerformers);
 
     // hour 5
+    //minPQ of companys stockdetails for 5th hour.
     MinPQ<Stock> hour5Min = new MinPQ<Stock>();
+    //maxPQ of companys stockdetails for 5th hour.
     MaxPQ<Stock> hour5Max = new MaxPQ<Stock>();
 
     for (int i = 0; i < stockSize; i++) {
       String[] inputs = scan.nextLine().split(",");
+      //Initialing temp and storing the company names and percentages.
       Stock temp = new Stock(inputs[0], Double.parseDouble(inputs[1]));
+      //inserting temp int max.
       hour5Max.insert(temp);
+      //inserting temp int min.
       hour5Min.insert(temp);
     }
-
+    //printing bestperformers for hour5.
     hour5Max.print(bestPerformers);
+    //printing worstperformers for hour5.
     hour5Min.print(worstPerformers);
 
     // hour 6
+    //minPQ of companys stockdetails for 6th hour.
     MinPQ<Stock> hour6Min = new MinPQ<Stock>();
+    //maxPQ of companys stockdetails for 6th hour.
     MaxPQ<Stock> hour6Max = new MaxPQ<Stock>();
 
     for (int i = 0; i < stockSize; i++) {
       String[] inputs = scan.nextLine().split(",");
+      //Initialing temp and storing the company names and percentages.
       Stock temp = new Stock(inputs[0], Double.parseDouble(inputs[1]));
+      //inserting temp int max.
       hour6Max.insert(temp);
+      //inserting temp int min.
       hour6Min.insert(temp);
     }
-
+    //printing bestperformers for hour6.
     hour6Max.print(bestPerformers);
+    //printing worstperformers for hour6.
     hour6Min.print(worstPerformers);
   }
 }
