a7456d7f8cc7998222d94eb51665a36dca18142a
pvsteja <tejapathri6@msitprogram.net>
2018-10-27 12:11:18 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS1 Assignments/M24/1/Solution.java
+++ ADS1 Assignments/M24/1/Solution.java
@@ -17,7 +17,7 @@ public final class Solution {
    * @param      args  The arguments
    */
   public static void main(final String[] args) {
-  	//creating new Scanner from system.
+    //creating new Scanner from system.
     Scanner sc = new Scanner(System.in);
     // a new line consists of int which will be stored in n.
     int n = Integer.parseInt(sc.nextLine());
@@ -25,8 +25,8 @@ public final class Solution {
     LinearProbingHashST<Integer, String> st =
      new LinearProbingHashST<Integer, String>();
     for (int i = 0; i < n; i++) {
-    	// String array tokens means rollnumbers of students which are in
-    	// String array will be split with (,) in the next line.
+        // String array tokens means rollnumbers of students which are in
+        // String array will be split with (,) in the next line.
       String[] tokens = sc.nextLine().split(",");
       //String s consists of names and grades with comma seperated.
       String s = tokens[1] + "," + tokens[2];
@@ -37,7 +37,7 @@ public final class Solution {
     int m = Integer.parseInt(sc.nextLine());
     // this loop will repeat m times.
     for (int j = 0; j < m; j++) {
-    	// tokens1 in string array will be split with spaces in next line.
+        // tokens1 in string array will be split with spaces in next line.
       String[] tokens1 = sc.nextLine().split(" ");
       String s = st.get(Integer.parseInt(tokens1[1]));
       // if string s is null then print "students doesn't exist"
@@ -46,10 +46,10 @@ public final class Solution {
         // if tokens == 1 then print name of the student else
         // print the grades of the respective rollnumbers.
         if (Integer.parseInt(tokens1[2]) == 1) {
-        	//printing names.
+            //printing names.
           System.out.println(tokens2[0]);
         } else {
-        	//printing marks.
+            //printing marks.
           System.out.println(Double.parseDouble(tokens2[1]));
         }
       } else {
